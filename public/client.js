798bbdbe7ad3a98b3a22863954cb3aad6bda4ccf
Matt Coppola <matt.coppola@gmail.com>
2018-11-16 19:11:09 -0600

    added signup form modal, added js for open and close function

--- public/client.js
+++ public/client.js
@@ -1,9 +1,17 @@
-$('a').on('click', function (e) {
+///////////////////////////////////////////
+//on.click Button Scroll to next Section //
+///////////////////////////////////////////
+
+$('.btn').on('click', function (e) {
     e.preventDefault();
     $('html, body').animate({
         scrollTop: $($(this).attr('href')).offset().top
     }, 500, 'linear');
 });
+
+///////////////////////////////////////
+//Modal Functions and jQuery Actions //
+///////////////////////////////////////
 
 $('.login').on('click', function (e) {
     e.preventDefault();
@@ -17,6 +25,17 @@ $('.login-close').on('click', function (e) {
     closeLoginModal();
 });
 
+$('.signup').on('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    showSignupModal();
+});
+
+$('.signup-close').on('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    closeSignupModal();
+});
 
 function showLoginModal() {
     $('#login').css('display', 'block');
@@ -24,4 +43,12 @@ function showLoginModal() {
 
 function closeLoginModal() {
     $('#login').css('display', 'none');
+}
+
+function showSignupModal() {
+    $('#signup').css('display', 'block');
+}
+
+function closeSignupModal() {
+    $('#signup').css('display', 'none');
 }
